03d9db8bc9a1903f9edcb870f2b3e9f5d6d21ff7
Alexey Dubnov <alex4972@mail.ru>
2025-12-02 18:56:40 +0200

    02.12

--- Script/script4props.js
+++ Script/script4props.js
@@ -1,24 +1,18 @@
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const Student = ({name, age}) => {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <h2>age: {age ?? 'hidden'}</h2>
-    </div>
-  )
+    return (
+        <div>
+            <h1>{name}</h1>
+            <h2>age: {age ?? 'hidden'}</h2>
+        </div>
+    )
 }
 
 root.render(
-  <div>
-    <Student
-      name={"John"}
-      age={20}
-    />
-    <Student
-      name={'Peter'}
-      age={25}
-    />
-    <Student name={'Mary'} />
-  </div>
+    <div>
+        <Student name={"John"} age={20}/>
+        <Student name={'Peter'} age={25}/>
+        <Student name={'Mary'}/>
+    </div>
 );
--- Script/script6state.js
+++ Script/script6state.js
@@ -1,18 +1,18 @@
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function CheckBox() {
-  const [isChecked, setIsChecked] = React.useState(false);
+    const [isChecked, setIsChecked] = React.useState(false);
 
-  return (
-    <div>
-      <input
-        type={'checkbox'}
-        defaultChecked={isChecked}
-        onChange={() => setIsChecked(!isChecked)}
-      />
-      <p>{isChecked ? 'checked' : 'unchecked'}</p>
-    </div>
-  )
+    return (
+        <div>
+            <input
+                type={'checkbox'}
+                defaultChecked={isChecked}
+                onChange={() => setIsChecked(!isChecked)}
+            />
+            <p>{isChecked ? 'checked' : 'unchecked'}</p>
+        </div>
+    )
 }
 
-root.render(<CheckBox />);
+root.render(<CheckBox/>);
